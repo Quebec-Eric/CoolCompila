84f730a201e7b04c928878fd8f11afc223d8e3e3
Quebec-Eric <quebec.ericjs@gmail.com>
2023-11-10 10:01:10 -0300

    Semant.cc

--- assignments/PA4/semant.h
+++ assignments/PA4/semant.h
@@ -2,37 +2,44 @@
 #define SEMANT_H_
 
 #include <assert.h>
-#include <iostream>  
+#include <iostream>
 #include "cool-tree.h"
 #include "stringtab.h"
 #include "symtab.h"
 #include "list.h"
+#include <map>
+#include <vector>
 
 #define TRUE 1
 #define FALSE 0
 
 class ClassTable;
 typedef ClassTable *ClassTableP;
+typedef SymbolTable<Symbol, Symbol> ObjectEnvironment;
+typedef std::map<Class_, std::vector<method_class *>> MethodTable;
 
-// This is a structure that may be used to contain the semantic
-// information such as the inheritance graph.  You may use it or not as
-// you like: it is only here to provide a container for the supplied
-// methods.
 
 class ClassTable {
 private:
-  int semant_errors;
-  void install_basic_classes();
-  ostream& error_stream;
+
 
 public:
-  ClassTable(Classes);
+ int semant_errors;
+  void install_basic_classes();
+  ostream &error_stream;
+  ObjectEnvironment objectEnv;
+  MethodTable methodTable;
+  std::map<Symbol, Class_> classTable;
+  void check_main();
+  void check_methods();
+  void install_methods();
+  bool conform(Symbol name1, Symbol name2);
+  Class_ LCA(Symbol name1, Symbol name2);
+ ClassTable(Classes);
   int errors() { return semant_errors; }
-  ostream& semant_error();
-  ostream& semant_error(Class_ c);
-  ostream& semant_error(Symbol filename, tree_node *t);
+  ostream &semant_error();
+  ostream &semant_error(Class_ c);
+  ostream &semant_error(Symbol filename, tree_node *t);
 };
 
-
 #endif
-
